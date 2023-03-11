4efaa4dc4b9bca88a04cb06113982e85d2e479e5
Marisol Ospina <mov281215@gmail.com>
2023-03-11 13:02:31 -0500

    changes in html and css, data

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import { filterByName, sortByName, filterByType } from "./data.js";
 
 import data from "./data/pokemon/pokemon.js";
 
-console.log(sortByName(data.pokemon));
-console.log(filterByType(data.pokemon));
-console.log(filterByName(data.pokemon, "sq"));
-
 const cardFront = document.querySelector(".card-front-container");
 
 function displayPokemon(pokemonData) {
@@ -15,7 +11,9 @@ function displayPokemon(pokemonData) {
     card.innerHTML = `
       <div class="pokemon-num">#${pokemon.num}</div>
       <img class="pokemon-image" src="${pokemon.img}" alt="${pokemon.name}">
-      <div class="pokemon-name">${pokemon.name}</div>
+      <div class="pokemon-name">${
+        pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+      }</div>
       <div class="pokemon-type">${pokemon.type}</div>
     `;
     cardFront.appendChild(card);
