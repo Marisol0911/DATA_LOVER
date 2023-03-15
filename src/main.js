b23a927cce4628a48e15c4780500f1efdff12b91
Marisol Ospina <mov281215@gmail.com>
2023-03-15 15:39:34 -0500

    add function to filter pokemons on keydown

--- src/main.js
+++ src/main.js
@@ -1,25 +1,20 @@
 import { filterByName, sortByName, filterByType } from "./data.js";
 
 import data from "./data/pokemon/pokemon.js";
-console.log(sortByName(data.pokemon));
-//console.log(filterByType(data.pokemon, "grass"));
-console.log(filterByName(data.pokemon, "ditto"));
-const cardFront = document.querySelector(".card-front-container");
 
-function displayPokemon(pokemonData) {
-  pokemonData.pokemon.forEach((pokemon) => {
-    const card = document.createElement("div");
-    card.classList.add("pokemon-card");
-    card.innerHTML = `
-      <div class="pokemon-num">#${pokemon.num}</div>
-      <img class="pokemon-image" src="${pokemon.img}" alt="${pokemon.name}">
-      <div class="pokemon-name">${
-        pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
-      }</div>
-      <div class="pokemon-type">${pokemon.type}</div>
-    `;
-    cardFront.appendChild(card);
-  });
+//selecion de elementos del DOM
+const inputName = document.getElementById("btnName");
+const cardToShow = document.getElementById("card-front");
+
+//console.log(sortByName(data.pokemon));
+//console.log(filterByType(data.pokemon, "grass"));
+//console.log(filterByName(data.pokemon, "ditto"));
+function printPokemonName(event) {
+  //alert("Hola mundo");
+  const value = event.target.value;
+
+  const pokemonsFiltered = filterByName(data.pokemon, value);
+  console.log(pokemonsFiltered);
 }
 
-displayPokemon(data);
+inputName.addEventListener("keydown", printPokemonName);
